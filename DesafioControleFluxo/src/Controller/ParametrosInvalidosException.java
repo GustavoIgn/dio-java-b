b2c2f5423c222a104b13509d387d1d5adaed013c
GustavoIgn <gustavoignacio280@gmail.com>
2024-07-29 12:31:54 -0300

    Desafio concluido

---
+++ DesafioControleFluxo/src/Controller/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package Controller;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
